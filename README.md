ca33bfc76e2e8e43c93c978299b5ca2a74fb082c
hackerman <sam@aflorithmic.ai>
2023-12-09 21:16:46 +0000

    all days

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TODO
+
+This year I went for speed over other metrics. Some readability has suffered as a result. 
--- gen_folders.sh
+++ gen_folders.sh
@@ -4,10 +4,10 @@ start_index=1
 end_index=25
 for ((i=start_index; i<=end_index; i++)); do
   # format a dirpath with the 3-digits index
-  printf -v dirpath 'solutions/%d/1' $i
+  printf -v dirpath 'solutions/%d' $i
   mkdir -p -- "$dirpath"
-  printf -v dirpath 'solutions/%d/2' $i
-  mkdir -p -- "$dirpath"
+  # printf -v dirpath 'solutions/%d/2' $i
+  # mkdir -p -- "$dirpath"
   printf -v dirpath 'solutions/%d/notes.md' $i
   touch "$dirpath"
 done
